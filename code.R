45eb6e05fdb4e616fdad32c6f9ac5ad03ef592fc
Gabriel <gabrielrubinho05@gmail.com>
2023-03-23 22:52:56 -0300

    versao-final

--- code.R
+++ code.R
@@ -1,4 +1,4 @@
-maentel_haeszer_test <- function(x, correct = FALSE) { 
+mantel_haenszel_test <- function(x, correct = c(TRUE, FALSE), confidence_level) { 
   dimensao <- as.list(dim(x))[[3]]
   
   lista <- list()
@@ -11,12 +11,10 @@ maentel_haeszer_test <- function(x, correct = FALSE) {
     (x[1, 1]*x[2, 2])/sum(x)
   }
   
- 
   ratio2 <- function(x) {
     (x[1, 2]*x[2, 1])/sum(x)
   }
   
-
   soma1 <- Reduce('+', lapply(lista, ratio)) # Certo
   soma2 <- Reduce('+', lapply(lista, ratio2)) # Certo
   
@@ -28,22 +26,12 @@ maentel_haeszer_test <- function(x, correct = FALSE) {
   }
   
   soma_f <- Reduce('+', lapply(lista, f_statistic)) # Certo
-
-  
-  # R = (ai*di)/ni
-  
-  r_statistic_squared <- function(x) {
-    ((x[1, 1]*x[2, 2])/(x[1,1]+x[2,2]+x[2,1]+x[1,2]))^2
-  }
   
   r_statistic <- function(x) {
     (x[1,1]*x[2,2])/(x[1,1]+x[2,2]+x[2,1]+x[1,2])
   }
   
-  soma_r_squared <- Reduce('+', lapply(lista, r_statistic_squared)) # Certo
   soma_r         <- Reduce('+', lapply(lista, r_statistic)) # Certo
-  
-  # G = ((ai*bi(bi+ci)) + (bi*ci(aidi))/ni^2
   
   g_statistic <- function(x) {
     (x[1,1]*x[2,2]*(x[1,2]+x[2,1])+x[1,2]*x[2,1]*(x[1,1]+x[2,2]))/(x[1,1]+x[2,2]+x[1,2]+x[2,1])^2
@@ -51,15 +39,12 @@ maentel_haeszer_test <- function(x, correct = FALSE) {
   }
   
   soma_g <- Reduce('+', lapply(lista, g_statistic)) # Certo
-
-  # H = (bici(bi+ci))/ni^2
+  
   
   h_statistic <- function(x) {
     ((x[1, 2]*x[2, 1])*(x[1, 2]+x[2, 1]))/(x[1,1]+x[2,2]+x[2,1]+x[1,2])^2
   }
-  #fantoche
   
-  #((bi*ci)*(bi+ci))/(sum(fantoche)^2)
   
   soma_h <- Reduce('+', lapply(lista, h_statistic)) # Certo
   
@@ -67,41 +52,46 @@ maentel_haeszer_test <- function(x, correct = FALSE) {
     (x[1, 2]*x[2, 1])/sum(x)
   }
   
-  s_statistic_squared <- function(x) {
-    ((x[1, 2]*x[2, 1])/sum(x))^2
-  }
   
   soma_s <- Reduce('+', lapply(lista, s_statistic)) # Certo
-  soma_s_squared <- Reduce('+', lapply(lista, s_statistic_squared)) # Certo
   
   # Todas estão certas..
   
   
-  var_odds.ratio <- soma_f/(2*soma_r_squared) + soma_g/(2*soma_r*soma_s) + soma_h/(2*soma_s_squared)
-  var_ln.odds.ratio <- var_odds.ratio/(odds_ratio_estimator^2)
-  
-  ic_superior <- exp(log(odds_ratio_estimator) + 1.96*sqrt(var_ln.odds.ratio))
-  ic_inferior <- exp(log(odds_ratio_estimator) - 1.96*sqrt(var_ln.odds.ratio))
+  var_odds.ratio <- round(soma_f/(2*(soma_r^2)), digits = 4) + 
+    round(soma_g/(2*soma_r*soma_s), digits = 4) + 
+    round(soma_h/(2*(soma_s^2)), digits = 4)
   
   
+  var_ln.odds.ratio <- var_odds.ratio/(odds_ratio_estimator^2) # Certo
+  
+  significance_level <- 1-confidence_level
+  z <- 1 - significance_level/2
+  z_calculator <- 1 - z
+  
+  
+  ic_superior <- exp(log(odds_ratio_estimator) + qnorm(z_calculator, lower.tail = FALSE)*sqrt(var_ln.odds.ratio)) # Certo
+  ic_inferior <- exp(log(odds_ratio_estimator) - qnorm(z_calculator, lower.tail = FALSE)*sqrt(var_ln.odds.ratio)) # Certo
+  
+  # Tudo pra cima tá certo..
   estimator_variance <- function(x) {
-    ((x[1, 1]+x[1, 2])*(x[2, 1]+x[2, 2])*(x[1, 1]*x[2, 1])*(x[1, 2]+x[2, 2]))/((sum(x)^2)*(sum(x)-1))
+    ((x[1,1]+x[1,2])*(x[1,1]+x[2,1])*(x[1,2]+x[2,2])*(x[2,1]+x[2,2]))/(sum(x)^2*(sum(x)-1))
   }
   
-  soma_estimador_variance <- Reduce('+', lapply(lista, estimator_variance))
+  soma_estimador_variance <- Reduce('+', lapply(lista, estimator_variance)) # Certo
   
   expectation_estimator <- function(x) {
     ((x[1, 1]+x[1, 2])*(x[1, 1]+x[2, 1]))/sum(x)
   }
   
-  soma_expectation_estimator <- Reduce('+', lapply(lista, expectation_estimator))
+  soma_expectation_estimator <- Reduce('+', lapply(lista, expectation_estimator)) # Certo
   
-  soma_a <- function(x) {
+  soma_linha_1coluna_1 <- function(x) {
     x[1, 1]
   }
   
-  sum_a <- Reduce('+', lapply(lista, soma_a))
-  paste0(ic_inferior, ic_superior)
+  sum_a <- Reduce('+', lapply(lista, soma_a)) # Certo
+  
   
   if (correct == TRUE) {
     mantel_haenszer_statistic <- ((abs(sum_a - soma_expectation_estimator) - 0.5)^2)/soma_estimador_variance
@@ -114,19 +104,25 @@ maentel_haeszer_test <- function(x, correct = FALSE) {
   # Que é o valor do qui-quadrado tabelado para 1 grau de liberdade e o nível de significância escolhido (retirado do seu material)
   
   if (correct == TRUE) {
-    print('Mantel-Haenszel chi-squared test with continuity correction')
+    print('Teste de Mantel-Haenszel com correção de continuidade')
   } else {
-    print('Mantel-Haenszel chi-squared test without continuity correction')
+    print('Teste de Mantel-Haenszel sem correção de continuidade')
   }
   
-  p_value <- pchisq(mantel_haenszer_statistic, 1, lower.tail = FALSE)
+  p_value <- 1 - pchisq(mantel_haenszer_statistic, 1)
   
+  print(paste0("Estatística do teste: ", round(mantel_haenszer_statistic,4), 
+               ', gl = 1, valor-p: ', round(p_value,4)))
   
-  paste0('Mantel-Haenszel X-squared = ', mantel_haenszer_statistic, 'df = 1, p-value: ', p_value)
-  print('Alternative hypothesis: true common odds ratio is not equal to 1')
-  print('95 percent confidence interval: ')
-  print(ic_inferior, ic_superior)
-  paste0('Common odds ratio: ', odds_ratio_estimator)
+  print('Hipótese alternativa: true common odds ratio não é 1')
+  
+  print(paste0('Intervalo de confiança para ', confidence_level*100, '%'))
+  print(paste0(round(ic_inferior, digits = 4), '    ', round(ic_superior, digits = 4)))
+  
+  print(paste0('Valor crítico: à direita de ', round(qchisq(confidence_level, 1), 2)))
+  
+  print(paste0('Odds Ratio: ', round(odds_ratio_estimator, digits = 2)))
+  
   
   if (p_value  < 0.05) {
     print('Como o p valor é menor do que 0.05, rejeitamos a hipótese nula')
@@ -136,11 +132,4 @@ maentel_haeszer_test <- function(x, correct = FALSE) {
   
 }
 
-data(Titanic, package = 'datasets')
-partial_tables <- margin.table(Titanic, c(2,4,1))
-mantelhaen.test(partial_tables)
-maentel_haenszer_test(partial_tables)
 
-teste31 <- array(c(1011, 390, 81, 77, 383, 365, 66, 123), dim = c(2, 2, 2))
-teste31
-maentel_haeszer_test(teste31)
